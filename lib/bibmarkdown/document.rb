3e7ac3fd3978e3f5c21546fc9ac6aa910c0259c2
Ruben Verborgh <ruben@verborgh.org>
2016-11-26 16:25:23 +0100

    Add reference links.

--- lib/bibmarkdown/document.rb
+++ lib/bibmarkdown/document.rb
@@ -8,16 +8,28 @@ module BibMarkdown
     end
 
     def to_markdown
+      reference_ids = {}
+
       # Replace all citations by links
       markdown = @source.gsub %r{\[([^\]]+)\]\(cite:(\w+)\s+([^\)]+)\)} do |match|
         html = $1; rel = $2; key = $3
+
+        # Look up or assign reference ID
+        if reference_ids.has_key? key
+          reference_id = reference_ids[key]
+        else
+          reference_id = reference_ids[key] = reference_ids.length + 1
+        end
 
         # Look up citation and its URL
         entry = @entries[key]
         url = entry[:url]
 
         # Create the citation link
-        create_link html, url, 'http://purl.org/spar/cito/' + rel
+        link = create_link html, url, rel: 'http://purl.org/spar/cito/' + rel
+        reflink = create_link "[#{reference_id}]", "#ref-#{reference_id}", class: 'reference'
+
+        "#{link} #{reflink}"
       end
     end
 
@@ -26,8 +38,10 @@ module BibMarkdown
       CGI::escapeHTML(text)
     end
 
-    def create_link html, url, rel
-      %Q{<a href="#{h url}" rel="#{h rel}">#{html}</a>}
+    def create_link html, url, attrs
+      attrs[:href] = url
+      attrs = attrs.map { |attr, value| %Q{#{attr}="#{h value}"} }
+      %Q{<a #{attrs.join ' '}>#{html}</a>}
     end
   end
 end
