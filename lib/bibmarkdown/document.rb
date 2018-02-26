RubenVerborgh/BibMarkdown

commit 04add5db58d69ed9734600559b20ca21bc890e24

Fix multiple last names not sticking together
This fixes the issue where authors such as
"Van de Sompel, Herbert" were displayed as "Sompel, H. Van de".
While individual CSL templates could fix this
with demote-non-dropping-particle="never", most of them don't.

lib/bibmarkdown/document.rb
@@ -114,9 +114,20 @@ module BibMarkdown
     end
 
     def reference_html key
-      # Render reference
+      # Prepare reference HTML renderer
       processor = CiteProc::Processor.new style: @style, format: 'html'
-      processor << @entries[key].to_citeproc
+      citeproc = @entries[key].to_citeproc
+
+      # Ensure multi-part last names stick together
+      (citeproc["author"] || citeproc["editor"]).each do |author|
+        if author.has_key? "non-dropping-particle"
+          author["family"] = "#{author["non-dropping-particle"]} #{author["family"]}"
+          author.delete "non-dropping-particle"
+        end
+      end
+
+      # Render reference
+      processor << citeproc
       citations = processor.render :bibliography, id: key
       citation = citations.first
 

commit c785e23a4c7fb83513495cf40e375ea14d42c4d7

Fix crash for references without author or editor
04add5db58d69ed9734600559b20ca21bc890e24 introduced a crash for references without an author or editor.

lib/bibmarkdown/document.rb
@@ -119,7 +119,7 @@ module BibMarkdown
       citeproc = @entries[key].to_citeproc
 
       # Ensure multi-part last names stick together
-      (citeproc["author"] || citeproc["editor"]).each do |author|
+      (citeproc["author"] || citeproc["editor"] || []).each do |author|
         if author.has_key? "non-dropping-particle"
           author["family"] = "#{author["non-dropping-particle"]} #{author["family"]}"
           author.delete "non-dropping-particle"